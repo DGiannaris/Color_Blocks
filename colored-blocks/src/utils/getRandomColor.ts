3da81d1c4806cdae285ba377e3949c709c17fff9
DGiannaris <mtsgiannaris@gmail.com>
2022-05-13 21:55:20 +0300

    utils: imporove getRandomColor to work with hex colors

--- colored-blocks/src/utils/getRandomColor.ts
+++ colored-blocks/src/utils/getRandomColor.ts
@@ -2,8 +2,8 @@ import { Color } from '../common/types.ts'
 
 export const getRandomColor = (currentColor: number, colors: Color[]) => {
   const colorPool = colors.reduce((acc, next) => {
-    if (next.name !== currentColor) {
-      acc.push(next.name)
+    if ((next.name || next.hex) !== currentColor) {
+      acc.push(next.name || next.hex)
     }
     return acc;
   }, [])
